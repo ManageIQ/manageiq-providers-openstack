e9cb9cfc3c45c1ec1dde1778645d730584e48784
Tzu-Mainn Chen <tzumainn@redhat.com>
2017-04-21 16:50:50 +0200

    removed unneeded duplicated code from base snapshot concern

--- app/models/manageiq/providers/openstack/cloud_manager/vm/operations/snapshot.rb
+++ app/models/manageiq/providers/openstack/cloud_manager/vm/operations/snapshot.rb
@@ -2,15 +2,7 @@ module ManageIQ::Providers::Openstack::CloudManager::Vm::Operations::Snapshot
   extend ActiveSupport::Concern
 
   included do
-    supports :snapshot_create do
-      if supports_snapshots?
-        unless supports_control?
-          unsupported_reason_add(:snapshot_create, unsupported_reason(:control))
-        end
-      else
-        unsupported_reason_add(:snapshot_create, _("Operation not supported"))
-      end
-    end
+    supports :snapshot_create
 
     supports :remove_snapshot do
       if supports_snapshots?
@@ -32,105 +24,11 @@ module ManageIQ::Providers::Openstack::CloudManager::Vm::Operations::Snapshot
     end
 
     supports :remove_snapshot_by_description do
-      unless supports_remove_snapshot?
-        unsupported_reason_add(:remove_snapshot_by_description, unsupported_reason(:remove_snapshot))
-      end
+      unsupported_reason_add(:remove_snapshot_by_description, _("Operation not supported"))
     end
-  end
 
-  def raw_create_snapshot(name, desc = nil, memory = false)
-    run_command_via_parent(:vm_create_snapshot, :name => name, :desc => desc, :memory => memory)
-  end
-
-  def create_snapshot(name, desc = nil, memory = false)
-    check_policy_prevent(:request_vm_create_snapshot, :raw_create_snapshot, name, desc, memory)
-  end
-
-  def raw_remove_snapshot(snapshot_id)
-    raise MiqVmError, unsupported_reason(:remove_snapshot) unless supports_remove_snapshot?
-    snapshot = snapshots.find_by(:id => snapshot_id)
-    raise _("Requested VM snapshot not found, unable to remove snapshot") unless snapshot
-    begin
-      run_command_via_parent(:vm_remove_snapshot, :snMor => snapshot.uid_ems)
-    rescue => err
-      if err.to_s.include?('not found')
-        raise MiqVmSnapshotError, err.to_s
-      else
-        raise
-      end
+    supports :revert_to_snapshot do
+      unsupported_reason_add(:revert_to_snapshot, _("Operation not supported"))
     end
-  end
-
-  #
-  # For some types of VMs, the process for removing
-  # evm stapshots is very different from that of
-  # removing normal snapshots.
-  #
-  # Here, we differentiate between the two, so the
-  # methods can be overridden by the subclass as needed.
-  #
-
-  def remove_snapshot(snapshot_id)
-    raw_remove_snapshot(snapshot_id)
-  end
-
-  def remove_evm_snapshot(snapshot_id)
-    raw_remove_snapshot(snapshot_id)
-  end
-
-  def remove_snapshot_queue(snapshot_id, task_id = nil)
-    MiqQueue.put_unless_exists(
-      :class_name  => self.class.name,
-      :instance_id => id,
-      :method_name => 'remove_snapshot',
-      :args        => [snapshot_id],
-      :role        => "ems_operations",
-      :zone        => my_zone,
-      :task_id     => task_id
-    )
-  end
-
-  def remove_evm_snapshot_queue(snapshot_id, task_id = nil)
-    MiqQueue.put_unless_exists(
-      :class_name  => self.class.name,
-      :instance_id => id,
-      :method_name => 'remove_evm_snapshot',
-      :args        => [snapshot_id],
-      :role        => "ems_operations",
-      :zone        => my_zone,
-      :task_id     => task_id
-    )
-  end
-
-  def raw_remove_snapshot_by_description(description, refresh = false)
-    raise MiqVmError, unsupported_reason(:remove_snapshot_by_description) unless supports_remove_snapshot_by_description?
-    run_command_via_parent(:vm_remove_snapshot_by_description, :description => description, :refresh => refresh)
-  end
-
-  def remove_snapshot_by_description(description, refresh = false, retry_time = nil)
-    if (ext_management_system.kind_of?(ManageIQ::Providers::Vmware::InfraManager) && ManageIQ::Providers::Vmware::InfraManager.use_vim_broker? && MiqVimBrokerWorker.available?) || host.nil? || host.state == "on"
-      raw_remove_snapshot_by_description(description, refresh)
-    else
-      if retry_time.nil?
-        raise _("The VM's Host system is unavailable to remove the snapshot. VM id:[%{id}] Snapshot description:[%{description}]") % {:id => id, :description => description}
-      end
-      # If the host is off re-queue the action based on the retry_time
-      MiqQueue.put(:class_name  => self.class.name,
-                   :instance_id => id,
-                   :method_name => 'remove_snapshot_by_description',
-                   :args        => [description, refresh, retry_time],
-                   :deliver_on  => Time.now.utc + retry_time,
-                   :role        => "smartstate",
-                   :zone        => my_zone)
-    end
-  end
-
-  def raw_remove_all_snapshots
-    raise MiqVmError, unsupported_reason(:remove_all_snapshots) unless supports_remove_all_snapshots?
-    run_command_via_parent(:vm_remove_all_snapshots)
-  end
-
-  def remove_all_snapshots
-    raw_remove_all_snapshots
   end
 end
