69ed2fa2cd324490532c0f042847711576e7deb2
Petr Blaho <pblaho@redhat.com>
2017-08-28 18:00:29 +0200

    Adds with_notification method to unify usage of Notifications
    Adds with_notification method witch unifies the usage of Notifications
    Uses ActiveSupport::Concern with HelperMethods

--- app/models/manageiq/providers/openstack/helper_methods.rb
+++ app/models/manageiq/providers/openstack/helper_methods.rb
@@ -9,6 +9,10 @@ module ManageIQ::Providers::Openstack::HelperMethods
     self.class.parse_error_message_from_neutron_response(exception)
   end
 
+  def with_notification(type, options: {}, &block)
+    self.class.with_notification(type, :options => options, &block)
+  end
+
   module ClassMethods
     def parse_error_message_from_fog_response(exception)
       exception_string = exception.to_s
@@ -18,6 +22,26 @@ module ManageIQ::Providers::Openstack::HelperMethods
 
     def parse_error_message_from_neutron_response(exception)
       JSON.parse(exception.response.body)["NeutronError"]["message"]
+    end
+
+    def with_notification(type, options: {})
+      # extract success and error options from options
+      # :success and :error keys respectively
+      # with all other keys common for both cases
+      success_options = options.delete(:success) || {}
+      error_options = options.delete(:error) || {}
+      success_options.merge!(options)
+      error_options.merge!(options)
+      begin
+        yield
+      rescue => ex
+        # Fog specific
+        error_message = parse_error_message_from_fog_response(ex.to_s)
+        Notification.create(:type => "#{type}_error".to_sym, :options => error_options.merge(:error_message => error_message))
+        raise
+      else
+        Notification.create(:type => "#{type}_success".to_sym, :options => success_options)
+      end
     end
   end
 end
