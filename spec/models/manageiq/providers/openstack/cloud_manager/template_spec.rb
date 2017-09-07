cd39a4e77dbb8dae0c077afcff32527b47af26eb
Andrei Veselov <aveselov@redhat.com>
2017-09-07 11:08:05 +0200

    Add tests for Image delete action

---
+++ spec/models/manageiq/providers/openstack/cloud_manager/template_spec.rb
@@ -0,0 +1,26 @@
+describe ManageIQ::Providers::Openstack::CloudManager::Template do
+  let(:ems) { FactoryGirl.create(:ems_openstack_with_authentication) }
+  let(:template_openstack) { FactoryGirl.create :template_openstack, :ext_management_system => ems, :ems_ref => 'one_id' }
+  let(:service) { double }
+
+  context 'when raw_delete_image' do
+    before do
+      allow(ExtManagementSystem).to receive(:find).with(ems.id).and_return(ems)
+      allow(ems).to receive(:with_provider_connection).with(:service => 'Compute').and_yield(service)
+    end
+
+    subject { template_openstack }
+
+    it 'should delete image' do
+      expect(service).to receive(:delete_image).with(template_openstack.ems_ref).once
+      subject.delete_image
+    end
+
+    it 'should raise error' do
+      allow(service).to receive(:delete_image).with(template_openstack.ems_ref).and_raise(Excon::Error::BadRequest)
+      expect do
+        subject.delete_image
+      end.to raise_error(MiqException::MiqOpenstackApiRequestError)
+    end
+  end
+end
