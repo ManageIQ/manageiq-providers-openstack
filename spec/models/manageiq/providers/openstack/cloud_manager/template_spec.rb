71f29b3e95597e94a5ca64bbb7a041e73c9a0c50
Andrei Veselov <aveselov@redhat.com>
2017-09-19 17:05:31 +0200

    Add tests for Image create action

--- spec/models/manageiq/providers/openstack/cloud_manager/template_spec.rb
+++ spec/models/manageiq/providers/openstack/cloud_manager/template_spec.rb
@@ -1,8 +1,39 @@
 describe ManageIQ::Providers::Openstack::CloudManager::Template do
   let(:ems) { FactoryGirl.create(:ems_openstack_with_authentication) }
+  let(:image_attributes) { {:name => "image", :ram => "1"} }
   let(:template_openstack) { FactoryGirl.create :template_openstack, :ext_management_system => ems, :ems_ref => 'one_id' }
   let(:service) { double }
 
+  context 'when create_image' do
+    before do
+      allow(ems).to receive(:with_provider_connection).with(:service => 'Image').and_yield(service)
+    end
+
+    context 'with correct data' do
+      it 'should create image' do
+        expect(service).to receive(:create_image).with(image_attributes).once
+        subject.class.create_image(ems, image_attributes)
+      end
+
+      it 'should not raise error' do
+        allow(service).to receive(:create_image).with(image_attributes).once
+        expect do
+          subject.class.create_image(ems, image_attributes)
+        end.not_to raise_error
+      end
+    end
+
+    context 'with incorrect data' do
+      [Excon::Error::BadRequest, ArgumentError].map do |error|
+        it "should raise error when #{error}" do
+          allow(service).to receive(:create_image).with(image_attributes).and_raise(error)
+          expect do
+            subject.class.create_image(ems, image_attributes)
+          end.to raise_error(MiqException::MiqOpenstackApiRequestError)
+        end
+      end
+    end
+  end
   context 'when raw_delete_image' do
     before do
       allow(ExtManagementSystem).to receive(:find).with(ems.id).and_return(ems)
