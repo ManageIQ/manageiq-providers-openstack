618d9d5b2a0cd81c8b4ec13fec9e0b40fdaa377c
Bill Wei <bilwei@redhat.com>
2017-10-03 16:35:36 -0400

    Fix reference to factory vnfd_template_openstack_in_yaml

--- spec/models/manageiq/providers/openstack/cloud_manager/vnfd_template_spec.rb
+++ spec/models/manageiq/providers/openstack/cloud_manager/vnfd_template_spec.rb
@@ -7,7 +7,7 @@ describe ManageIQ::Providers::Openstack::CloudManager::VnfdTemplate do
     end
   end
 
-  let(:valid_template) { FactoryGirl.create(:orchestration_template_vnfd_with_content) }
+  let(:valid_template) { FactoryGirl.create(:vnfd_template_openstack_in_yaml) }
 
   describe '#validate_format' do
     it 'passes validation if no content' do
