268c221d9a6db7f75259e31fe45ed7b68be071d5
Tzu-Mainn Chen <tzumainn@redhat.com>
2017-10-23 21:48:59 +0200

    removed whitespace

--- app/models/manageiq/providers/openstack/network_manager/event_parser.rb
+++ app/models/manageiq/providers/openstack/network_manager/event_parser.rb
@@ -3,10 +3,10 @@ module ManageIQ::Providers::Openstack::NetworkManager::EventParser
     content = message_content(event, ems_id)
     event_type = content["event_type"]
     payload = content["payload"]
-    
+
     log_header = "ems_id: [#{ems_id}] " unless ems_id.nil?
     _log.debug("#{log_header}event: [#{event_type}]") if $log && $log.debug?
-    
+
     # attributes that are common to all notifications
     event_hash = {
       :event_type => event_type,
@@ -17,14 +17,14 @@ module ManageIQ::Providers::Openstack::NetworkManager::EventParser
       :full_data  => event,
       :ems_id     => ems_id
     }
-    
+
     event_hash[:vm_ems_ref] = payload["instance_id"]                      if payload.key? "instance_id"
     event_hash[:host_ems_ref] = payload["host"]                           if payload.key? "host"
     event_hash[:availability_zone_ems_ref] = payload["availability_zone"] if payload.key? "availability_zone"
     event_hash[:chain_id] = payload["reservation_id"]                     if payload.key? "reservation_id"
     event_hash
   end
-  
+
   def self.message_content(event, ems_id)
     unless ems_id.nil?
       ems = ExtManagementSystem.find_by_id(ems_id)
