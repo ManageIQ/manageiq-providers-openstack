e6ee2a051f891407f612b216533612e40f23c6a9
Alexander Demichev <ademicev@redhat.com>
2017-11-06 14:39:59 +0100

    replace conditions with scope

---
+++ spec/models/manageiq/providers/openstack/infra_manager/host_service_group_spec.rb
@@ -0,0 +1,20 @@
+describe ManageIQ::Providers::Openstack::InfraManager::HostServiceGroup do
+  let(:host_service_group) { FactoryGirl.create(:host_service_group_openstack) }
+
+  describe "return scopes" do
+    it "calls Filesystem scope" do
+      expect(Filesystem).to receive(:host_service_group_filesystems).with(host_service_group.id)
+      host_service_group.host_service_group_filesystems
+    end
+
+    it "calls SystemService scope with running systemd" do
+      expect(SystemService).to receive(:host_service_group_running_systemd).with(host_service_group.id)
+      host_service_group.running_system_services
+    end
+
+    it "calls SystemService scope with failed systemd" do
+      expect(SystemService).to receive(:host_service_group_failed_systemd).with(host_service_group.id)
+      host_service_group.failed_system_services
+    end
+  end
+end
