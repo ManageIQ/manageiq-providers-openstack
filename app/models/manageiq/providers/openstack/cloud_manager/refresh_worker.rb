690deb49a7983cbf25433b686b8c3e32423243d1
Sam Lucidi <slucidi@redhat.com>
2017-11-10 13:32:36 -0500

    Consolidate Openstack refresh workers

--- app/models/manageiq/providers/openstack/cloud_manager/refresh_worker.rb
+++ app/models/manageiq/providers/openstack/cloud_manager/refresh_worker.rb
@@ -1,6 +1,22 @@
 class ManageIQ::Providers::Openstack::CloudManager::RefreshWorker < ::MiqEmsRefreshWorker
   require_nested :Runner
 
+  # overriding queue_name_for_ems so PerEmsWorkerMixin picks up *all* of the
+  # Openstack-manager types from here.
+  # This way, the refresher for Openstack's CloudManager will refresh *all*
+  # of the Openstack inventory across all managers.
+  def self.queue_name_for_ems(ems)
+    if ems.kind_of?(ExtManagementSystem)
+      ["ems_#{ems.id}"] + ems.child_managers.collect { |manager| "ems_#{manager.id}" }
+    else
+      super
+    end
+  end
+
+  # MiQ complains if this isn't defined
+  def queue_name_for_ems(ems)
+  end
+
   def self.ems_class
     ManageIQ::Providers::Openstack::CloudManager
   end
