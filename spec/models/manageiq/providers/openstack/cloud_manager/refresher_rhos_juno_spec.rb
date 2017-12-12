d2fdf9fe96dbfc4427f86863ea5ee6e4bd13b5e9
Sam Lucidi <slucidi@redhat.com>
2017-12-12 10:45:41 -0500

    Fix openstack settings location

--- spec/models/manageiq/providers/openstack/cloud_manager/refresher_rhos_juno_spec.rb
+++ spec/models/manageiq/providers/openstack/cloud_manager/refresher_rhos_juno_spec.rb
@@ -37,8 +37,8 @@ describe ManageIQ::Providers::Openstack::CloudManager::Refresher do
 
   context "when using an admin account for fast refresh" do
     it "will perform a fast full refresh against RHOS #{@environment}" do
-      ::Settings.ems.ems_refresh.openstack.is_admin = true
-      ::Settings.ems.ems_refresh.openstack_network.is_admin = true
+      ::Settings.ems_refresh.openstack.is_admin = true
+      ::Settings.ems_refresh.openstack_network.is_admin = true
       2.times do
         with_cassette("#{@environment}_fast_refresh", @ems) do
           EmsRefresh.refresh(@ems)
@@ -49,16 +49,16 @@ describe ManageIQ::Providers::Openstack::CloudManager::Refresher do
 
         assert_common
       end
-      ::Settings.ems.ems_refresh.openstack.is_admin = false
-      ::Settings.ems.ems_refresh.openstack_network.is_admin = false
+      ::Settings.ems_refresh.openstack.is_admin = false
+      ::Settings.ems_refresh.openstack_network.is_admin = false
     end
   end
 
   it "will perform a fast full legacy refresh against RHOS #{@environment}" do
-    ::Settings.ems.ems_refresh.openstack.is_admin = true
-    ::Settings.ems.ems_refresh.openstack_network.is_admin = true
-    ::Settings.ems.ems_refresh.openstack.inventory_object_refresh = false
-    ::Settings.ems.ems_refresh.openstack_network.inventory_object_refresh = false
+    ::Settings.ems_refresh.openstack.is_admin = true
+    ::Settings.ems_refresh.openstack_network.is_admin = true
+    ::Settings.ems_refresh.openstack.inventory_object_refresh = false
+    ::Settings.ems_refresh.openstack_network.inventory_object_refresh = false
     2.times do
       with_cassette("#{@environment}_legacy_fast_refresh", @ems) do
         EmsRefresh.refresh(@ems)
@@ -69,10 +69,10 @@ describe ManageIQ::Providers::Openstack::CloudManager::Refresher do
 
       assert_common
     end
-    ::Settings.ems.ems_refresh.openstack.is_admin = false
-    ::Settings.ems.ems_refresh.openstack_network.is_admin = false
-    ::Settings.ems.ems_refresh.openstack.inventory_object_refresh = true
-    ::Settings.ems.ems_refresh.openstack_network.inventory_object_refresh = true
+    ::Settings.ems_refresh.openstack.is_admin = false
+    ::Settings.ems_refresh.openstack_network.is_admin = false
+    ::Settings.ems_refresh.openstack.inventory_object_refresh = true
+    ::Settings.ems_refresh.openstack_network.inventory_object_refresh = true
   end
 
   context "when paired with a infrastructure provider" do
