1e060626baa0241490a8729332012eb45f72bbcf
Sam Lucidi <slucidi@redhat.com>
2018-01-16 15:32:19 -0500

    Refresh volumes along with targeted vms

--- app/models/manageiq/providers/openstack/inventory/collector/storage_manager/cinder_manager.rb
+++ app/models/manageiq/providers/openstack/inventory/collector/storage_manager/cinder_manager.rb
@@ -3,16 +3,16 @@ class ManageIQ::Providers::Openstack::Inventory::Collector::StorageManager::Cind
 
   def cloud_volumes
     return @cloud_volumes if @cloud_volumes.any?
-    @cloud_volumes = cinder_service.handled_list(:volumes)
+    @cloud_volumes = cinder_service.handled_list(:volumes, {}, cinder_admin?)
   end
 
   def cloud_volume_snapshots
     return @cloud_volume_snapshots if @cloud_volume_snapshots.any?
-    @cloud_volume_snapshots = cinder_service.handled_list(:list_snapshots_detailed, :__request_body_index => "snapshots")
+    @cloud_volume_snapshots = cinder_service.handled_list(:list_snapshots_detailed, {:__request_body_index => "snapshots"}, cinder_admin?)
   end
 
   def cloud_volume_backups
     return @cloud_volume_backups if @cloud_volume_backups.any?
-    @cloud_volume_backups = cinder_service.list_backups_detailed.body["backups"]
+    @cloud_volume_backups = cinder_service.handled_list(:list_backups_detailed, {:__request_body_index => "backups"}, cinder_admin?)
   end
 end
