49c59f8c5b97ad1418099f732f3b59ee4013e4b4
James Wong <webpotent@gmail.com>
2018-01-17 14:18:59 -0500

    Provider base class handles the managers' destroy now

--- app/models/manageiq/providers/openstack/provider.rb
+++ app/models/manageiq/providers/openstack/provider.rb
@@ -6,7 +6,6 @@ class ManageIQ::Providers::Openstack::Provider < ::Provider
   has_many :cloud_ems,
            :foreign_key => "provider_id",
            :class_name  => "ManageIQ::Providers::Openstack::CloudManager",
-           :dependent   => :nullify,
            :autosave    => true
   has_many :network_managers,
            :foreign_key => "provider_id",
