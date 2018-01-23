9bd97e0f56ca78e934ef991862f4387844426e41
Gilles Dubreuil <gilles@redhat.com>
2018-01-23 12:46:06 +1100

    Use common symbol for infra

--- app/models/manageiq/providers/openstack/infra_discovery.rb
+++ app/models/manageiq/providers/openstack/infra_discovery.rb
@@ -3,15 +3,15 @@ require 'manageiq/network_discovery/port_scanner'
 module ManageIQ::Providers::Openstack
   class InfraDiscovery
     def self.probe(ost)
-      res = ""
       if ManageIQ::NetworkDiscovery::PortScanner.portOpen(ost, 6385)
+        res = ""
         Socket.tcp(ost.ipaddr, 6385) do |s|
           s.print("GET / HTTP/1.0\r\n\r\n")
           s.close_write
           res = s.read
         end
+        ost.hypervisor << :openstack_infra if res =~ /OpenStack Ironic API/
       end
-      ost.hypervisor << :ospinfra if res =~ /OpenStack Ironic API/
     end
   end
 end
