3da8ec69ef44ba8b0c7d519f4d91ff169aa055e6
Sam Lucidi <sam@samlucidi.com>
2018-01-31 18:00:31 -0500

    Don't require CinderManager in inventory classes

--- app/models/manageiq/providers/openstack/inventory/parser.rb
+++ app/models/manageiq/providers/openstack/inventory/parser.rb
@@ -1,5 +1,4 @@
 class ManageIQ::Providers::Openstack::Inventory::Parser < ManagerRefresh::Inventory::Parser
   require_nested :CloudManager
   require_nested :NetworkManager
-  require_nested :CinderManager
 end
