acd671244ff4eeb3b6b9f4f9714f76eb905809c1
Marek Aufart <aufi.cz@gmail.com>
2018-02-01 10:31:53 +0100

    Merge pull request #215 from gildub/network_discovery-autoload
    autoload move
    (cherry picked from commit 6d2e56636b745745968d45ac27982b8a884e8e04)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1552665

--- lib/manageiq-providers-openstack.rb
+++ lib/manageiq-providers-openstack.rb
@@ -1,2 +1,10 @@
 require "manageiq/providers/openstack/engine"
 require "manageiq/providers/openstack/version"
+
+module ManageIQ
+  module Providers
+    module Openstack
+      autoload :InfraDiscovery, 'manageiq/providers/openstack/infra_discovery'
+    end
+  end
+end
