c3afd13fbff7df3a25133481c2fb5025527b78de
Ladislav Smola <lsmola@redhat.com>
2018-04-27 16:49:37 +0200

    Merge pull request #280 from mansam/handled-list-catch-fog-not-found
    Catch Fog::Errors::NotFound in OpenstackHandle.handled_list
    (cherry picked from commit a95440e8ae0acea9667458ee3af3c0eb26ecb765)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1572760

--- lib/manageiq/providers/openstack/legacy/openstack_handle/handled_list.rb
+++ lib/manageiq/providers/openstack/legacy/openstack_handle/handled_list.rb
@@ -35,7 +35,7 @@ module OpenstackHandle
                 "type: #{collection_type}, in provider: #{@os_handle.address}. Message=#{err.message}"
       _log.warn err.backtrace.join("\n")
       []
-    rescue Excon::Errors::NotFound => err
+    rescue Excon::Errors::NotFound, Fog::Errors::NotFound => err
       # It can happen that some data do not exist anymore, in that case log warning but continue refresh
       _log.warn "Data not found in project: #{@os_handle.project_name}, for collection type: #{collection_type}, "\
                 "in provider: #{@os_handle.address}. Message=#{err.message}"
