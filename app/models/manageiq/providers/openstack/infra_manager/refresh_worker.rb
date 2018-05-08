5864c40f0c4833c04f210f68aab2cc6d306b4ce9
Sam Lucidi <slucidi@redhat.com>
2018-05-08 14:44:46 -0400

    Combine InfraManager and child manager refresh queues
    When the CloudManager's refresh queues were consolidated,
    the individual NetworkManager refresh worker was removed.
    However, the InfraManager's queues were not combined at
    the same time, resulting in Infra network refresh events
    not being picked up. This commit ensures that the Infra
    Manager's refresh worker picks up queued refreshes for
    its child managers as well.
    
    Fixes https://bugzilla.redhat.com/show_bug.cgi?id=1572760

--- app/models/manageiq/providers/openstack/infra_manager/refresh_worker.rb
+++ app/models/manageiq/providers/openstack/infra_manager/refresh_worker.rb
@@ -5,6 +5,27 @@ class ManageIQ::Providers::Openstack::InfraManager::RefreshWorker < ::MiqEmsRefr
     ManageIQ::Providers::Openstack::InfraManager
   end
 
+  # overriding queue_name_for_ems so PerEmsWorkerMixin picks up *all* of the
+  # Openstack-manager types from here.
+  # This way, the refresher for Openstack's InfraManager will refresh *all*
+  # of the Openstack inventory across all managers.
+  class << self
+    def queue_name_for_ems(ems)
+      return ems unless ems.kind_of?(ExtManagementSystem)
+      combined_managers(ems).collect(&:queue_name).sort
+    end
+
+    private
+
+    def combined_managers(ems)
+      [ems].concat(ems.child_managers)
+    end
+  end
+
+  # MiQ complains if this isn't defined
+  def queue_name_for_ems(ems)
+  end
+
   def self.settings_name
     :ems_refresh_worker_openstack_infra
   end
