522a5f855f27d9117ddeb00797154751517ed3c3
Alexander Demichev <ademicev@redhat.com>
2018-07-24 16:09:14 +0200

    require hostname if provider is enabled

--- app/models/manageiq/providers/openstack/cloud_manager.rb
+++ app/models/manageiq/providers/openstack/cloud_manager.rb
@@ -54,6 +54,10 @@ class ManageIQ::Providers::Openstack::CloudManager < ManageIQ::Providers::CloudM
 
   before_update :ensure_managers_zone_and_provider_region
 
+  def hostname_required?
+    enabled?
+  end
+
   def ensure_managers_zone_and_provider_region
     if network_manager
       network_manager.zone_id         = zone_id
