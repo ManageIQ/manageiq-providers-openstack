218582aee8210f77c084d2a1e4d8a4ebcc6e46c9
Gilles Dubreuil <gilles@redhat.com>
2018-07-26 13:43:01 +1000

    Infra Discovery: support for SSL
    Adds discovery of ssl ports
    Adds default ironic ssl port

--- spec/models/manageiq/providers/openstack/discovery_spec.rb
+++ spec/models/manageiq/providers/openstack/discovery_spec.rb
@@ -2,9 +2,15 @@ describe ManageIQ::Providers::Openstack::Discovery do
   it ".probe" do
     require 'ostruct'
     allow(ManageIQ::NetworkDiscovery::Port).to receive(:open?).and_return(true)
-    allow(Socket).to receive(:tcp).and_yield(double(:print => nil, :close_write => nil, :read => "OpenStack Ironic API"))
-    ost = OpenStruct.new(:ipaddr => "172.168.0.1", :hypervisor => [])
+
+    allow(OpenSSL::SSL::SSLSocket).to receive(:new).and_raise(OpenSSL::SSL::SSLError)
+
+    tcp_client = double(:syswrite => nil, :close => nil, :close_write => nil, :read => "OpenStack Ironic API")
+    allow(TCPSocket).to receive(:new).with('172.168.0.1', 6385).and_return(tcp_client)
+    allow(TCPSocket).to receive(:new).with('172.168.0.1', 13_385).and_return(tcp_client)
+
+    ost = OpenStruct.new(:ipaddr => '172.168.0.1', :hypervisor => [])
     described_class.probe(ost)
-    expect(ost.hypervisor).to eq [:openstack_infra]
+    expect(ost.hypervisor).to eq %i(openstack_infra openstack_infra)
   end
 end
