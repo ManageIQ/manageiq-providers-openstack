250e17a8f817088e761195ca2f62f6446bfd1683
Alexander Demichev <ademicev@redhat.com>
2018-07-31 15:17:41 +0200

    Set tenant access when adding non public flavor

--- app/models/manageiq/providers/openstack/cloud_manager/flavor.rb
+++ app/models/manageiq/providers/openstack/cloud_manager/flavor.rb
@@ -3,7 +3,13 @@ class ManageIQ::Providers::Openstack::CloudManager::Flavor < ::Flavor
 
   def self.raw_create_flavor(ext_management_system, create_options)
     ext_management_system.with_provider_connection({:service => 'Compute'}) do |service|
-      service.flavors.create(create_options)
+      cloud_tenant_refs = create_options.delete("cloud_tenant_refs")
+      flavor = service.flavors.create(create_options)
+      unless flavor.is_public
+        cloud_tenant_refs.each do |cloud_tenant_ref|
+          service.add_flavor_access(flavor.id, cloud_tenant_ref)
+        end
+      end
     end
   rescue => err
     _log.error "flavor=[#{name}], error=[#{err}]"
