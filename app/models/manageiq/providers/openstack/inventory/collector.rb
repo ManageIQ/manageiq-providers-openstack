a4f15176ea475083c6b44d5b6e9f24a83465f825
Sam Lucidi <slucidi@redhat.com>
2018-09-11 17:37:53 -0400

    Add StorageManager modules to fix CinderManager import paths

--- app/models/manageiq/providers/openstack/inventory/collector.rb
+++ app/models/manageiq/providers/openstack/inventory/collector.rb
@@ -4,7 +4,7 @@ class ManageIQ::Providers::Openstack::Inventory::Collector < ManageIQ::Providers
 
   require_nested :CloudManager
   require_nested :NetworkManager
-  require_nested :CinderManager
+  require_nested :StorageManager
   require_nested :TargetCollection
 
   attr_reader :availability_zones
