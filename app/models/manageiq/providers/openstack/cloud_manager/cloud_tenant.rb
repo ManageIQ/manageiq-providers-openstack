ebac53e0f0a6aa164eeb331c6b328cbb5001ae39
Aparna Karve <akarve@redhat.com>
2018-09-12 12:02:34 -0700

    make default_security_group visible in the API

--- app/models/manageiq/providers/openstack/cloud_manager/cloud_tenant.rb
+++ app/models/manageiq/providers/openstack/cloud_manager/cloud_tenant.rb
@@ -1,4 +1,6 @@
 class ManageIQ::Providers::Openstack::CloudManager::CloudTenant < ::CloudTenant
+  virtual_has_one :default_security_group, :uses => :security_groups
+
   include ManageIQ::Providers::Openstack::HelperMethods
   has_and_belongs_to_many :miq_templates,
                           :foreign_key             => "cloud_tenant_id",
