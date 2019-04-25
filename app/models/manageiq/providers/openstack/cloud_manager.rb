371a0c69fc678eaa3da8ac79e417b872c71b5dcc
Marek Aufart <aufi.cz@gmail.com>
2019-04-25 14:19:11 +0200

    Merge pull request #462 from mansam/overcloud-snapshot-creation
     Create instance snapshots with the owner's project scope
    
    (cherry picked from commit da83ccad7e4914b61a1550f056ca9818fe808b11)
    
    Fixes https://bugzilla.redhat.com/show_bug.cgi?id=1703074

--- app/models/manageiq/providers/openstack/cloud_manager.rb
+++ app/models/manageiq/providers/openstack/cloud_manager.rb
@@ -266,20 +266,11 @@ class ManageIQ::Providers::Openstack::CloudManager < ManageIQ::Providers::CloudM
   def vm_create_snapshot(vm, options = {})
     log_prefix = "vm=[#{vm.name}]"
 
-    miq_openstack_instance = MiqOpenStackInstance.new(vm.ems_ref, openstack_handle)
-    snapshot = miq_openstack_instance.create_snapshot(options)
+    compute_service = openstack_handle.compute_service(vm.cloud_tenant.name)
+    snapshot = compute_service.create_image(vm.ems_ref, options[:name], :description => options[:desc]).body["image"]
+
     Notification.create(:type => :vm_snapshot_success, :subject => vm, :options => {:snapshot_op => 'create'})
     snapshot_id = snapshot["id"]
-
-    # Add new snapshot to the snapshots table.
-    vm.snapshots.create!(
-      :name        => options[:name],
-      :description => options[:desc],
-      :uid         => snapshot_id,
-      :uid_ems     => snapshot_id,
-      :ems_ref     => snapshot_id,
-      :create_time => snapshot["created"]
-    )
 
     return snapshot_id
   rescue => err
