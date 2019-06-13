975457827a7d2f20e654b37bb1d6618ec965b3f7
Joe Rafaniello <jrafanie@redhat.com>
2019-06-13 10:16:55 -0400

    Pass id to AR::Base exists?/find, not AR::Base objs
    This is rails 5.0 and 5.1 safe.
    
    On Rails 5.1, this raises an ArgumentError.
    
    ```
      1) ManageIQ::Providers::Openstack::IdentitySyncMixin create_or_find_miq_group_add_user should create new group and add user as member
         Failure/Error: expect(miq_group.users.exists?(user)).to be true
    
         ArgumentError:
           You are passing an instance of ActiveRecord::Base to `exists?`. Please pass the id of the object by calling `.id`.
         # /Users/joerafaniello/.gem/ruby/2.5.5/gems/activerecord-5.1.7/lib/active_record/relation/finder_methods.rb:304:in `exists?'
         # ./spec/models/manageiq/providers/openstack/identity_sync_mixin_spec.rb:120:in `block (3 levels) in <top (required)>'
    
      3) ManageIQ::Providers::Openstack::CloudManager::Vm when resized initiate resize process
         Failure/Error: vm.resize(flavor)
    
         ArgumentError:
           You are passing an instance of ActiveRecord::Base to `find`. Please pass the id of the object by calling `.id`.
         # /Users/joerafaniello/.gem/ruby/2.5.5/gems/activerecord-5.1.7/lib/active_record/relation/finder_methods.rb:451:in `find_one'
         # /Users/joerafaniello/.gem/ruby/2.5.5/gems/activerecord-5.1.7/lib/active_record/relation/finder_methods.rb:440:in `find_with_ids'
         # /Users/joerafaniello/.gem/ruby/2.5.5/gems/activerecord-5.1.7/lib/active_record/relation/finder_methods.rb:66:in `find'
         # /Users/joerafaniello/.gem/ruby/2.5.5/gems/activerecord-5.1.7/lib/active_record/querying.rb:3:in `find'
         # /Users/joerafaniello/.gem/ruby/2.5.5/gems/activerecord-5.1.7/lib/active_record/core.rb:171:in `find'
         # /Users/joerafaniello/Code/manageiq/app/models/manageiq/providers/cloud_manager/vm.rb:112:in `resize'
         # ./spec/models/manageiq/providers/openstack/cloud_manager/vm_spec.rb:242:in `block (3 levels) in <top (required)>'
    ```

--- spec/models/manageiq/providers/openstack/cloud_manager/vm_spec.rb
+++ spec/models/manageiq/providers/openstack/cloud_manager/vm_spec.rb
@@ -239,7 +239,7 @@ describe ManageIQ::Providers::Openstack::CloudManager::Vm do
       expect(vm.validate_resize_confirm).to be false
       expect(service).to receive(:resize_server).with(vm.ems_ref, flavor.ems_ref)
       expect(MiqQueue).to receive(:put)
-      vm.resize(flavor)
+      vm.resize(flavor.id)
     end
 
     it 'confirm resize' do
