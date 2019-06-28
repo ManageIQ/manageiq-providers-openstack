f253fe349e1a1cf9e4ad053c1a99c7bf5061af30
d-m-u <drewuhlmann@gmail.com>
2019-06-28 15:42:41 -0400

    The leaf class should be tested and there's no point in the other spec now

--- spec/models/manageiq/providers/openstack/cloud_manager/refresh_worker_spec.rb
+++ spec/models/manageiq/providers/openstack/cloud_manager/refresh_worker_spec.rb
@@ -1,28 +1,11 @@
 describe ManageIQ::Providers::Openstack::CloudManager::RefreshWorker do
   context "EMS with children" do
-    let!(:network_manager) { FactoryBot.create(:ems_network) }
-    let!(:storage_manager) { FactoryBot.create(:ems_storage) }
-    let(:ems) do
-      FactoryBot.create(:ems_cloud).tap do |ems|
-        network_manager.update_attributes(:parent_ems_id => ems.id)
-        storage_manager.update_attributes(:parent_ems_id => ems.id)
-      end
-    end
+    let(:ems) { FactoryBot.create(:ems_openstack) }
 
     it ".queue_name_for_ems" do
       queue_name = described_class.queue_name_for_ems(ems)
-      expect(queue_name.count).to eq(3)
+      expect(queue_name.count).to eq(4)
       expect(queue_name.sort).to  eq(queue_name)
-    end
-  end
-
-  context "EMS with no children" do
-    let(:ems) { FactoryBot.create(:ems_cloud) }
-
-    it ".queue_name_for_ems" do
-      queue_name = described_class.queue_name_for_ems(ems)
-      expect(queue_name.count).to eq(1)
-      expect(queue_name.first).to eq(ems.queue_name)
     end
   end
 end
