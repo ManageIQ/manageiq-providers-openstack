f841b5bf0ee9c03f1b170f81be804365999444c5
Oleg Barenboim <chessbyte@gmail.com>
2019-11-08 13:33:06 -0500

    Add and subclass scan_timeout_adjustment_multiplier method

--- app/models/manageiq/providers/openstack/cloud_manager/vm_or_template_shared/scanning.rb
+++ app/models/manageiq/providers/openstack/cloud_manager/vm_or_template_shared/scanning.rb
@@ -10,6 +10,19 @@ module ManageIQ::Providers::Openstack::CloudManager::VmOrTemplateShared::Scannin
     end
   end
 
+  #
+  # Adjustment Multiplier is 4 (i.e. 4 times the specified timeout)
+  #
+  # TODO: until we get location/offset read capability for OpenStack
+  #   image data, OpenStack scanning is prone to timeout (based on image size).
+  #
+  # Maybe this should be calculated based on the size of the image (on the instance method),
+  #   but that information isn't directly available.
+  #
+  def self.scan_timeout_adjustment_multiplier
+    4
+  end
+
   def require_snapshot_for_scan?
     false
   end
