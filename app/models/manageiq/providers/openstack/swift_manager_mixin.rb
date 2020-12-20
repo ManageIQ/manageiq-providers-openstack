4375f59b7fb22055bfa4a88ce4d201f27eba3f10
pyon-yx <codestar0714@outlook.com>
2020-12-20 20:28:51 +0800

    - "Configuration" button is enabled in Storage->Object Storage->Object Store Containers page. - New code for Openstack swift manager is added. CloudObjectStoreContainer class is defined, and raw_cloud_object_store_container_create function is called when customer tries to add new container for openstack swift manager.

--- app/models/manageiq/providers/openstack/swift_manager_mixin.rb
+++ app/models/manageiq/providers/openstack/swift_manager_mixin.rb
@@ -5,12 +5,12 @@ module ManageIQ::Providers::Openstack::SwiftManagerMixin
   included do
     # TODO: how about many storage managers???
     # Should use has_many :storage_managers,
-    has_one  :inverse_of,
-             :swift_manager,
-             :dependent,
-             :foreign_key => :parent_ems_id,
-             :class_name  => "ManageIQ::Providers::StorageManager::SwiftManager",
-             :autosave    => true
+    has_one :swift_manager,
+            :dependent,
+            :foreign_key => :parent_ems_id,
+            :inverse_of => false,
+            :class_name  => "ManageIQ::Providers::StorageManager::SwiftManager",
+            :autosave    => true
 
     delegate :cloud_object_container,
              :cloud_object_object,
