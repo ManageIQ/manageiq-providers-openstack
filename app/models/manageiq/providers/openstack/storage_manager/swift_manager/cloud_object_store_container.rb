a73afc743a592da7a485704e4ca13098f529eba1
pyon-yx <codestar0714@outlook.com>
2021-10-02 21:01:41 +0300

    fix some errors and warnings by pyon

--- app/models/manageiq/providers/openstack/storage_manager/swift_manager/cloud_object_store_container.rb
+++ app/models/manageiq/providers/openstack/storage_manager/swift_manager/cloud_object_store_container.rb
@@ -13,21 +13,21 @@ class ManageIQ::Providers::Openstack::StorageManager::SwiftManager::CloudObjectS
     {
       :fields => [
         {
-            :component    => 'select',
-            :name         => 'cloud_tenant_id',
-            :id           => 'cloud_tenant_id',
-            :label        => _('Cloud Tenant'),
-            :isRequired   => true,
-            :includeEmpty => true,
-            :validate     => [{:type => 'required'}],
-            :condition    => {
-                :when => 'edit',
-                :is   => false,
-            },
-            :options      => [{
-                  :label => cloud_tenants.name,
-                  :value => cloud_tenants.id,
-            }]
+          :component    => 'select',
+          :name         => 'cloud_tenant_id',
+          :id           => 'cloud_tenant_id',
+          :label        => _('Cloud Tenant'),
+          :isRequired   => true,
+          :includeEmpty => true,
+          :validate     => [{:type => 'required'}],
+          :condition    => {
+            :when => 'edit',
+            :is   => false,
+          },
+          :options      => [{
+                              :label => cloud_tenants.name,
+                              :value => cloud_tenants.id,
+                            }]
         }
       ]
     }
@@ -40,26 +40,26 @@ class ManageIQ::Providers::Openstack::StorageManager::SwiftManager::CloudObjectS
     options[:key] = options["name"]
     with_notification(:cloud_container_create,
                       :options => {
-                          :key => options["name"],
+                        :key => options["name"],
                       }) do
       ext_management_system.with_provider_connection(swift_connection_options(cloud_tenant)) do |service|
-        project_id = service.get_current_tenant()["id"]
+        project = service.get_current_tenant
+        project_id = project["id"]
         directory = service.directories.new(options)
         directory.save
       end
     end
 
-    {:ems_ref => project_id + "/" + options["name"],  :key => options["name"], :object_count => 0, :bytes => 0,
-     :ems_id => ext_management_system.id, :cloud_tenant_id => options["cloud_tenant_id"]}
-
+    {
+      :ems_ref => "#{project_id}/#{options["name"]}",
+      :key => options["name"],
+      :object_count => 0,
+      :bytes => 0,
+      :ems_id => ext_management_system.id,
+      :cloud_tenant_id => options["cloud_tenant_id"]
+    }
   rescue Exception => e
-    file_name = "provider_openstack_project"
-    File.open(Rails.root.join('public', 'upload', file_name), 'ab') do |file|
-      file.write(e.message)
-      file.write(e.backtrace.inspect)
-    end
-
-    _log.error "network=[#{options[:name]}], error: #{e}"
+    _log.error("container=[#{options["name"]}], error: #{e}")
     parsed_error = parse_error_message_from_neutron_response(e)
     raise MiqException::MiqCloudObjectStoreContainerCreateError, parsed_error, e.backtrace
   end
@@ -85,7 +85,7 @@ class ManageIQ::Providers::Openstack::StorageManager::SwiftManager::CloudObjectS
       file.write("delete cloud object store container options = #{options.to_s}\n")
     end
   rescue => e
-    _log.error "network=[#{name}], error: #{e}"
+    _log.error("network=[#{name}], error: #{e}")
     raise MiqException::MiqNetworkDeleteError, parse_error_message_from_neutron_response(e), e.backtrace
   end
 end
