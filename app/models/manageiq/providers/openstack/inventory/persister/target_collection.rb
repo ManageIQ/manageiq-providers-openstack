e73e0c38465074d9226fa07acffe7182da989a7c
Adam Grare <adam@grare.com>
2022-01-04 14:49:59 -0500

    Cleanup the TargetCollection persister

--- app/models/manageiq/providers/openstack/inventory/persister/target_collection.rb
+++ app/models/manageiq/providers/openstack/inventory/persister/target_collection.rb
@@ -7,26 +7,10 @@ class ManageIQ::Providers::Openstack::Inventory::Persister::TargetCollection < M
     true
   end
 
-  def strategy
-    :local_db_find_missing_references
-  end
-
-  def parent
-    if @init_network_collections
-      manager.try(:network_manager)
-    else
-      manager.presence
-    end
-  end
-
   def initialize_inventory_collections
     initialize_tag_mapper
     initialize_cloud_inventory_collections
-
-    @init_network_collections = true
     initialize_network_inventory_collections
-    @init_network_collections = false
-
     initialize_storage_inventory_collections
   end
 end
