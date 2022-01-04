f2285e9ff3525f69dc9a1c074554411f61fc9a89
Adam Grare <adam@grare.com>
2022-01-04 11:04:55 -0500

    Refactor openstack persister to use standard methods

--- app/models/manageiq/providers/openstack/inventory/persister/storage_manager/swift_manager.rb
+++ app/models/manageiq/providers/openstack/inventory/persister/storage_manager/swift_manager.rb
@@ -1,10 +1,15 @@
 class ManageIQ::Providers::Openstack::Inventory::Persister::StorageManager::SwiftManager < ManageIQ::Providers::Openstack::Inventory::Persister
   def initialize_inventory_collections
-    add_collection(storage, :cloud_object_store_objects)
-    add_collection(storage, :cloud_object_store_containers)
-
-    add_collection(cloud, :cloud_tenants, :parent => manager.parent_manager) do |builder|
+    add_storage_collection(:cloud_object_store_objects)
+    add_storage_collection(:cloud_object_store_containers)
+    add_cloud_collection(:cloud_tenants) do |builder|
       builder.add_properties(:strategy => :local_db_cache_all, :complete => false)
     end
+  end
+
+  private
+
+  def storage_manager
+    manager.kind_of?(EmsStorage) ? manager : manager.swift_manager
   end
 end
