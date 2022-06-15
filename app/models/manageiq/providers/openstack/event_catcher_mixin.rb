3061cdd9b97ef4eba5099bd7b720173d3a483f6d
Adam Grare <adam@grare.com>
2022-06-15 11:05:45 -0400

    Move all_valid_ems_in_zone to a mixin

---
+++ app/models/manageiq/providers/openstack/event_catcher_mixin.rb
@@ -0,0 +1,14 @@
+module ManageIQ::Providers::Openstack::EventCatcherMixin
+  extend ActiveSupport::Concern
+
+  class_methods do
+    def all_valid_ems_in_zone
+      require 'manageiq/providers/openstack/legacy/openstack_event_monitor'
+      super.select do |ems|
+        ems.sync_event_monitor_available?.tap do |available|
+          _log.info("Event Monitor unavailable for #{ems.name}.  Check log history for more details.") unless available
+        end
+      end
+    end
+  end
+end
