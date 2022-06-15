ea9273ce76207b9490e252d0f8e11e103d7d1bb8
Jason Frey <fryguy9@gmail.com>
2022-06-15 12:21:39 -0400

    Merge pull request #804 from agrare/move_all_valid_ems_in_zone_to_mixin
    Move all valid ems in zone to mixin
    
    (cherry picked from commit 4b0ff84bf641467e02ccbfe97463af4583be6e6d)

--- app/models/manageiq/providers/openstack/storage_manager/cinder_manager/event_catcher.rb
+++ app/models/manageiq/providers/openstack/storage_manager/cinder_manager/event_catcher.rb
@@ -1,16 +1,9 @@
 class ManageIQ::Providers::Openstack::StorageManager::CinderManager::EventCatcher < ::MiqEventCatcher
+  include ManageIQ::Providers::Openstack::EventCatcherMixin
+
   require_nested :Runner
 
   def self.settings_name
     :event_catcher_openstack_cinder
-  end
-
-  def self.all_valid_ems_in_zone
-    require 'manageiq/providers/openstack/legacy/openstack_event_monitor'
-    super.select do |ems|
-      ems.sync_event_monitor_available?.tap do |available|
-        _log.info("Event Monitor unavailable for #{ems.name}.  Check log history for more details.") unless available
-      end
-    end
   end
 end
