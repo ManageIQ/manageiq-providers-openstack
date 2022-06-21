94b789ae5d02836e217c564df24989afdd7d15d3
Adam Grare <adam@grare.com>
2022-06-21 15:46:30 -0400

    Check event_monitor_available before event catcher startup

--- app/models/manageiq/providers/openstack/event_catcher_mixin.rb
+++ app/models/manageiq/providers/openstack/event_catcher_mixin.rb
@@ -1,7 +1,15 @@
 module ManageIQ::Providers::Openstack::EventCatcherMixin
   extend ActiveSupport::Concern
 
-  def self.all_valid_ems_in_zone
-    super.select { |ems| ems.supports?(:events) }
+  def after_initialize
+    super
+
+    do_exit("EMS ID [#{@cfg[:ems_id]}] event monitor unavailable.", 1) unless ems.event_monitor_available?
+  end
+
+  class_methods do
+    def all_valid_ems_in_zone
+      super.select { |ems| ems.supports?(:events) }
+    end
   end
 end
