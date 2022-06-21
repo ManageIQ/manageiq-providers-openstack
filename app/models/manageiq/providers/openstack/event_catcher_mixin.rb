aa69d0057fc473f7ca7c6d8375a269774b25511e
Adam Grare <adam@grare.com>
2022-06-21 13:46:16 -0400

    Check Ceilometer service not just auth_status

--- app/models/manageiq/providers/openstack/event_catcher_mixin.rb
+++ app/models/manageiq/providers/openstack/event_catcher_mixin.rb
@@ -5,7 +5,7 @@ module ManageIQ::Providers::Openstack::EventCatcherMixin
     def all_valid_ems_in_zone
       require 'manageiq/providers/openstack/legacy/openstack_event_monitor'
       super.select do |ems|
-        ems.sync_event_monitor_available?.tap do |available|
+        ems.event_monitor_available?.tap do |available|
           _log.info("Event Monitor unavailable for #{ems.name}.  Check log history for more details.") unless available
         end
       end
