6f6621f1a97e35844854da4497c60d68e98b781b
Adam Grare <adam@grare.com>
2023-06-06 12:19:04 -0400

    Handle auth API without a token

--- spec/support/vcr_helper.rb
+++ spec/support/vcr_helper.rb
@@ -1,5 +1,7 @@
 def fix_token_expires_at(interaction)
   data = JSON.parse(interaction.response.body)
+  return if data.dig("token", "expires_at").nil?
+
   data["token"]["expires_at"] = "9999-12-31T23:59:59.999999Z"
   interaction.response.body = data.to_json.force_encoding('ASCII-8BIT')
 end
