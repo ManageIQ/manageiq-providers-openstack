1114d2ab3edb0397b9c48cd7f6a4ae6f9cd1e7c0
Adam Grare <adam@grare.com>
2023-06-13 13:17:09 -0400

    Use " vs ' consistently

--- spec/models/manageiq/providers/openstack/network_manager/cloud_network_spec.rb
+++ spec/models/manageiq/providers/openstack/network_manager/cloud_network_spec.rb
@@ -34,9 +34,9 @@ describe ManageIQ::Providers::Openstack::NetworkManager::CloudNetwork do
     raw_cloud_networks
   end
 
-  describe 'cloud network actions' do
+  describe "cloud network actions" do
     context ".create" do
-      it 'catches errors from provider' do
+      it "catches errors from provider" do
         expect(service).to receive_message_chain(:networks, :new).and_raise(bad_request)
         expect do
           ems_network.create_cloud_network(:cloud_tenant => tenant)
@@ -52,7 +52,7 @@ describe ManageIQ::Providers::Openstack::NetworkManager::CloudNetwork do
         cloud_network.update_cloud_network(options)
       end
 
-      it 'catches errors from provider' do
+      it "catches errors from provider" do
         expect(service).to receive(:update_network).and_raise(bad_request)
         expect { cloud_network.update_cloud_network({}) }.to raise_error(MiqException::MiqNetworkUpdateError)
       end
@@ -66,7 +66,7 @@ describe ManageIQ::Providers::Openstack::NetworkManager::CloudNetwork do
         cloud_network.delete_cloud_network({})
       end
 
-      it 'catches errors from provider' do
+      it "catches errors from provider" do
         expect(service).to receive(:delete_network).and_raise(bad_request)
         expect { cloud_network.delete_cloud_network({}) }.to raise_error(MiqException::MiqNetworkDeleteError)
       end
